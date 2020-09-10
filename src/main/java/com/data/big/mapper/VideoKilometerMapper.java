4c64b515fee5b3787f6fda874ddcf290efd6b782
“admin” <xxxxxx@449155439@>
2020-09-10 17:46:51 +0800

    重新生成workspace.xml

--- src/main/java/com/data/big/mapper/VideoKilometerMapper.java
+++ src/main/java/com/data/big/mapper/VideoKilometerMapper.java
@@ -2,9 +2,7 @@ package com.data.big.mapper;
 
 import com.data.big.base.BaseMapper;
 import com.data.big.model.VideoKilometer;
-import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
 public interface VideoKilometerMapper extends BaseMapper<VideoKilometer> {
     int insert(VideoKilometer record);
 
