49c6dfa66466154af617c68649d1be0ab384a520
“admin” <xxxxxx@449155439@>
2020-09-10 08:26:35 +0800

    增加web页面

--- src/main/java/com/data/big/controller/HtmlController.java
+++ src/main/java/com/data/big/controller/HtmlController.java
@@ -1,14 +1,33 @@
 package com.data.big.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 
 @Controller
 public class HtmlController {
     @RequestMapping("/login")
-    public String index(){
+    public String index(Model model){
+
+        Map map = new LinkedHashMap<>();
+        for (int i = 0; i < 5; i++) {
+            map.put("key" + i, "value" + i);
+        }
+        model.addAttribute("list", Arrays.asList("string1", "string2", "string3", "string4", "string5", "string6"));
+        model.addAttribute("map", map);
+        model.addAttribute("name", "   htTps://wWw.zHyD.mE   ");
+        model.addAttribute("htmlText", "<span style=\"color: red;font-size: 16px;\">html内容</span>");
+        model.addAttribute("num", 123.012);
+        model.addAttribute("null", null);
+        model.addAttribute("dateObj", new Date());
+        model.addAttribute("bol", true);
         return "login";
     }
 }
